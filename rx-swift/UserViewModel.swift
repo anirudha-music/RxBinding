5f86f369697e84ba7e077ef03be6d1b1cf4817fd
Anirudha Mahale <anirudhamusic.am@gmail.com>
2019-02-03 11:32:29 +0530

    Added some comments.

--- rx-swift/UserViewModel.swift
+++ rx-swift/UserViewModel.swift
@@ -14,6 +14,8 @@ class UserViewModel {
     let username = BehaviorRelay<String?>(value: nil)
     let names = BehaviorRelay<[String]>(value: [String]())
     
+    
+    /// Add new name to the datasource
     func add() {
         if let name = username.value, name != "" {
             self.names.accept(self.names.value + [name])
@@ -21,15 +23,18 @@ class UserViewModel {
         username.accept(nil)
     }
     
+    // removes all names from the datasource
     func clear() {
         names.accept([String]())
     }
     
+    // emits the selected name
     func selected(_ index: Int) {
         let name = names.value[index]
         username.accept(name)
     }
     
+    // removes the selected name from the data source
     func deleted(_ index: Int) {
         var dublicateNames = names.value
         dublicateNames.remove(at: index)
